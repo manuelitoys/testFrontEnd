7e7ed13feb8002baa673edc07a13b9ba91be5fa3
manuelitoys <manuelitoys93@gmail.com>
2023-05-24 22:15:54 -0500

    Hook Input
    Detection of the value on input text

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,18 +1,45 @@
+import { useState } from "react";
 
-function Search() {
+const _logo: string  = 'https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.22.8/mercadolibre/logo__large_plus.png';
 
-    const logo:string  = 'https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.22.8/mercadolibre/logo__large_plus.png';
+const searchProduct = (search: string) => {
+    console.log(search);
+    //TODO: Captura de informacion del input y busqueda en API
+}
+
+const Search = () => {
+
+    const [ search, setSearch ] = useState('');
 
     return(
         <>
             <div className="container containerSearch">
                 <div className="row mx-auto">
-                    <div className="col-3">
-                        <img src={logo} alt="" />
+                    <div className="col-2">
+                        <img src={_logo} alt="Logo Mercado Libre" />
                     </div>
-                    <div className="col-6">
+                    <div className="col-10">
                         <div className="inputGroup">
-                            <input type="text" placeholder='Nunca dejes de buscar' className="formControl"/>
+                            <form 
+                                className="form"
+                                onSubmit={ e => {
+                                    e.preventDefault();
+                                    searchProduct(search);
+                                } }
+                                >
+                                <input 
+                                    className="formControl" 
+                                    name="search"
+                                    placeholder='Nunca dejes de buscar' 
+                                    type="text" 
+                                    value={ search }
+                                    onChange={ e => setSearch(e.target.value) }
+                                    />
+                                <button 
+                                    className="btnSearch" 
+                                    type="submit" >
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -21,4 +48,4 @@ function Search() {
     )
 }
 
-export default Search
+export default Search;
