abedce8fcd615447de156e53ec6be080304fe0be
Manu <manuelitoys93@gmail.com>
2023-06-01 16:48:32 -0500

    configuracion para jest con typescript

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,27 +1,6 @@
-/*module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-          '<rootDir>/fileTransformer.cjs',
-      },
-}*/
-
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest/presets/js-with-ts',
-    testEnvironment: 'node',
-   /* globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>./tsconfig.json',
-        },
-    },*/
-    transform: {
-        "<transform_regex>": ['ts-jest', { tsconfig: '<rootDir>./tsconfig.json', }],},
-
-    "transform": {
-        "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest"
-      },
-      "transformIgnorePatterns": [
-        "node_modules/(?!variables/.*)"
-      ]
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: ["/node_modules/(?!query-string)"],  
+};
